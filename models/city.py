f20bf0ec25ae582fc42830dfd89a4b7f6589e41d
iammakena@gmail.com <root@MAKENA>
2024-05-01 01:17:29 +0300

    Adding a condition

--- models/city.py
+++ models/city.py
@@ -3,11 +3,23 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = "cities"
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    name = Column(String(128), nullable=False)
-    places = relationship("Place", backref="cities", cascade="delete")
+
+    if getenv("HBNB_ENV") == "db":
+        __tablename__ = "cities"
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities", cascade="delete")
+    else:
+        name = ""
+        state_id = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        this method initializes city.
+        """
+        super().__init__(*args, **kwargs)
