0c8d1836a6b6579cf1b6e722060a895e10373fb6
iammakena@gmail.com <root@MAKENA>
2024-05-01 01:30:13 +0300

    Reverted to old files

--- models/user.py
+++ models/user.py
@@ -3,29 +3,14 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    __tablename__ = "users"
 
-    if getenv("HBNB_ENV") == "db":
-        __tablename__ = "users"
-
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128))
-        last_name = Column(String(128))
-        places = relationship("Place", backref="user", cascade="delete")
-
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
-
-    def __init__(self, *args, *kwargs):
-        """
-        this method initializes user.
-        """
-        super().__init__(*args, *kwargs)
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
