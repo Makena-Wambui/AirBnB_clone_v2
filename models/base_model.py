9242efa3a56acf80e0a13d9627dd4f8a99b1bef9
Makena-Wambui <iammakena@gmail.com>
2024-07-27 10:56:23 +0300

    Fixing format of updated_at and created_at

--- models/base_model.py
+++ models/base_model.py
@@ -1,49 +1,61 @@
-#!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-
-import models
-from sqlalchemy import Column, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
-
-    id = Column(String(60), unique=True, nullable=False, primary_key=True)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        # if kwargs:
+        #     for key, value in kwargs.items():
+        #         if key == 'updated_at' or key == 'created_at':
+        #             value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+        #         if key != '__class__':
+        #             setattr(self, key, value)
+        # else:
+        #     self.id = str(uuid.uuid4())
+        #     self.created_at = self.updated_at = datetime.now()
 
-        if kwargs:
-            for key, val in kwargs.items():
-                if key == 'updated_at':
-                    val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
+        #     del kwargs['__class__']
+        #     self.__dict__.update(kwargs)
 
-                elif key == 'created_at':
-                    val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
-
-                elif key == "__class__":
-                    del val
-                else:
-                    setattr(self, key, val)
+        if not kwargs or "updated_at" not in kwargs \
+           and "created_at" not in kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            if kwargs:
+                self.__dict__.update(kwargs)
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.to_dict())
+        # Create a copy of __dict__ and remove '_sa_instance_state'
+        instance_dict = self.__dict__.copy()
+        if '_sa_instance_state' in instance_dict:
+            del instance_dict['_sa_instance_state']
+        return '[{}] ({}) {}'.format(cls, self.id, instance_dict)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
+        import models
         self.updated_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
@@ -61,9 +73,6 @@ class BaseModel:
         return dictionary
 
     def delete(self):
-        """
-        Add a new public instance method: def delete(self),
-        to delete the current instance from the storage (models.storage),
-        by calling the method delete.
-        """
-        models.storage.delete(self)
+        """Delete the current instance from the storage"""
+        from models import storage
+        storage.delete(self)
