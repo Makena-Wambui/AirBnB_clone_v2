9e56a25c49c3984ec203b5c06c8c4b2504d81818
Makena-Wambui <iammakena@gmail.com>
2024-07-27 09:20:57 +0300

    Fixed __str__

--- models/base_model.py
+++ models/base_model.py
@@ -39,7 +39,7 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        return '[{}] ({}) {}'.format(cls, self.id, self.to_dict())
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
@@ -50,13 +50,15 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        # lets change logic for removing this key:_sa_instance_state
-        d = self.__dict__.copy()
-        d.pop('_sa_instance_state', None)
-        d['__class__'] = str(self.__class__.__name__)
-        d['created_at'] = self.created_at.isoformat()
-        d['updated_at'] = self.updated_at.isoformat()
-        return d
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        dictionary.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        if "_sa_instance_state" in dictionary.keys():
+            del dictionary["_sa_instance_state"]
+        return dictionary
 
     def delete(self):
         """
