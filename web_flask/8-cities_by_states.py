ef358bdc61337837003bb43f5cf5b7e6baffb1b6
Makena-Wambui <iammakena@gmail.com>
2024-07-27 22:25:55 +0300

    Fix documentation

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
 """
-Script that starts a Flask web application
+Write a script that starts a Flask web application:
+
+Your web application must be listening on 0.0.0.0, port 5000
+You must use storage for fetching data from the storage engine
+    (FileStorage or DBStorage) =>
+    from models import storage and storage.all(...)
+To load all cities of a State:
+If your storage engine is DBStorage, you must use cities relationship
+Otherwise, use the public getter method cities
+After each request you must remove the current SQLAlchemy Session:
+    Declare a method to handle @app.teardown_appcontext
+    Call in this method storage.close()
+Routes:
+/cities_by_states: display a HTML page: (inside the tag BODY)
+H1 tag: “States”
+UL tag: with the list of all State objects sorted by name (
+LI tag: description of one State: <state.id>: <B><state.name></B> + UL tag:
+    with the list of City objects linked to the State sorted by name (A->Z)
+LI tag: description of one City: <city.id>: <B><city.name></B>
 """
 
 from flask import Flask, render_template
