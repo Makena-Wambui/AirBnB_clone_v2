eeca273fa9c6628fb8d928e4735bc35901003e75
Makena-Wambui <iammakena@gmail.com>
2024-07-27 21:02:34 +0300

    Better documentation

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,22 +1,41 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
 
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
 """
+Write a script that starts a Flask web application:
+
+Your web application must be listening on 0.0.0.0, port 5000
+use storage for fetching data from the storage engine, that is:
+    (FileStorage or DBStorage)
+    from models import storage and storage.all(...)
+
+After each request you must remove the current SQLAlchemy Session:
+Declare a method to handle @app.teardown_appcontext
+Call in this method storage.close()
+
+Routes:
+/states_list: display a HTML page: (inside the tag BODY)
+H1 tag: “States”
+UL tag: with the list of all State objects present in DBStorage(sort)
+LI tag: description of one State: <state.id>: <B><state.name></B>
+You must use the option strict_slashes=False in your route definition
+"""
+
 from models import storage
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
+    """
+    Func: states_list
+
+    Displays an HTML page with a list of all State objects.
 
     States are sorted by name.
+
+    Displayed as: <state_id>: <state_name>
     """
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
@@ -29,4 +48,4 @@ def teardown(exc):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port="5000")
